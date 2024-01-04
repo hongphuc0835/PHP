5d002a6bc4e871460d848ea053ca30449d2a9ccc
hongphuc0835 <143236536+hongphuc0835@users.noreply.github.com>
2024-01-04 22:37:34 +0700

    Add files via upload

---
+++ FORMS PHP/form handing welcome.php
@@ -0,0 +1,8 @@
+<?php
+if (isset($_POST["name"])){
+    echo "<p>welcome:".$_POST["name"]."</p>";
+
+//}
+//if (isset ($_POST["email"])){
+    echo "<p>your email address is:".$_POST["email"]."</p>";
+}
